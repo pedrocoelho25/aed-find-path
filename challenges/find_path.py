76507640a0feab8fb159e5680999d6b25931e4d5
pedrocoelho25 <joao.pedrocoelho@upe.br>
2026-06-25 13:39:21 -0300

    Update find_path.py

--- challenges/find_path.py
+++ challenges/find_path.py
@@ -1,4 +1,23 @@
+import heapq
 from data_structures.city_map import CityMap
+
+
+def estimar_distancia(city_map, atual, objetivo):
+    return 0
+
+
+def reconstruir_caminho(anterior, inicio, objetivo):
+    caminho = []
+    atual = objetivo
+
+    while atual != inicio:
+        caminho.append(atual)
+        atual = anterior[atual]
+
+    caminho.append(inicio)
+    caminho.reverse()
+
+    return caminho
 
 
 def find_path(
@@ -6,4 +25,52 @@ def find_path(
     start: int,
     goal: int,
 ) -> list[int]:
-    raise NotImplementedError
+
+    if start not in city_map.intersections:
+        return []
+
+    if goal not in city_map.intersections:
+        return []
+
+    if start == goal:
+        return [start]
+
+    fila_prioridade = []
+
+    heapq.heappush(fila_prioridade, (0, start))
+
+    custo_ate_agora = {}
+    custo_ate_agora[start] = 0
+
+    anterior = {}
+
+    visitados = set()
+
+    while len(fila_prioridade) > 0:
+        prioridade_atual, atual = heapq.heappop(fila_prioridade)
+
+        if atual in visitados:
+            continue
+
+        visitados.add(atual)
+
+        if atual == goal:
+            return reconstruir_caminho(anterior, start, goal)
+
+        vizinhos = city_map.roads[atual]
+
+        for vizinho in vizinhos:
+            novo_custo = custo_ate_agora[atual] + 1
+
+            if vizinho not in custo_ate_agora or novo_custo < custo_ate_agora[vizinho]:
+                custo_ate_agora[vizinho] = novo_custo
+
+                anterior[vizinho] = atual
+
+                estimativa = estimar_distancia(city_map, vizinho, goal)
+
+                prioridade = novo_custo + estimativa
+
+                heapq.heappush(fila_prioridade, (prioridade, vizinho))
+
+    return []
